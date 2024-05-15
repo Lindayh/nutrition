4944d55eaf19fdf15d72bbb354d5ad69439cef02
mariebayard <mariebayard87@gmail.com>
2024-05-15 14:33:09 +0200

    database and models, testroute, testhtml, added sql alchemy in requirements

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from models import db, Fruit
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///fruit_and_veg.db'
+db.init_app(app)
 
 @app.route("/")
 def home():
@@ -17,6 +20,11 @@ def mineraler():
 @app.route("/search")
 def search():
     return render_template("search.html") 
+
+@app.route("/test")
+def test():
+    frukter=Fruit.query.order_by(Fruit.Vitamin_A_RE_per_mikrog.desc()).limit(10).all()
+    return render_template('test.html', frukter=frukter)
 
 if __name__ == "__main__":
     app.run(debug=True)
