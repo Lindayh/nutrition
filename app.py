67c083147706af0bf83935db824ee1553ccf1db9
Linda Fogli <foglilinda@gmail.com>
2024-05-22 13:20:33 +0200

    vitamin info text

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from models import db, Fruit
 from sqlalchemy.orm import Session
+from vitamins_info import vitamins_info
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///fruit_and_veg.db'
@@ -29,40 +30,46 @@ def vitamin_info(vitamin):
     table = Fruit.__table__
     column_names = [column.name for column in table.columns]
 
-    for index,value in enumerate(column_names):
-        if value.startswith(vitamin_) or value.startswith(vitamin):
+    for index,col_name in enumerate(column_names):
+        if col_name.startswith(vitamin_) or col_name.startswith(vitamin):
             data = []
-            print(f"Column name: {value}")
+            print(f"Column name: {col_name}")
 
-            if value=="Niacin_mg" or value=="Niacinekvivalenter_NE_per_mg":
+            if col_name=="Niacin_mg" or col_name=="Niacinekvivalenter_NE_per_mg":
                 top_10 = Fruit.query.order_by((getattr(Fruit, "Niacinekvivalenter_NE_per_mg")).desc()).limit(10).all()
             else:
-                top_10 = Fruit.query.order_by((getattr(Fruit, value)).desc()).limit(10).all()  
+                top_10 = Fruit.query.order_by((getattr(Fruit, col_name)).desc()).limit(10).all()  
 
+            if "mg" in col_name.split("_"): unit = f"{vitamin} (mg)"
+            if "mikrog" in col_name.split("_"): unit = f"{vitamin} (mikrog)"
 
-            if "mg" in value.split("_"): unit = f"{vitamin} (mg)"
-            if "mikrog" in value.split("_"): unit = f"{vitamin} (mikrog)"
+            for idx, vitamin_dict in enumerate(vitamins_info):
+                vit_dict_name = vitamin_dict['name']
 
-            for index,val in enumerate(top_10):
-                temp_dict = {"name" : val.Namn,
-                             "vitamin_value" : getattr(val, value),
+                if vit_dict_name == vitamin:
+                    vitamin_text = []
+                    for key in vitamin_dict:
+                        if key!="name":
+                            vitamin_text.append(vitamin_dict[key])
+
+            for index,fruit_cls in enumerate(top_10):
+                temp_dict = {"name" : fruit_cls.Namn,
+                             "vitamin_value" : getattr(fruit_cls, col_name),
                              "unit": unit}
                 
                 if vitamin == "Vitamin A":
-                    temp_dict = {"name" : val.Namn,
-                             "vitamin_value" : getattr(val, value),
+                    temp_dict = {"name" : fruit_cls.Namn,
+                             "vitamin_value" : getattr(fruit_cls, col_name),
                              "unit": "Vitamin A (Retinolekvivalenter per mikgrogram)"}
                 
                 if vitamin == "Niacin":
-                    temp_dict = {"name" : val.Namn,
-                             "vitamin_value" : getattr(val, "Niacinekvivalenter_NE_per_mg"),
+                    temp_dict = {"name" : fruit_cls.Namn,
+                             "vitamin_value" : getattr(fruit_cls, "Niacinekvivalenter_NE_per_mg"),
                              "unit": "Niacin (Niacinekvivalenter per mg)"}
 
                 data.append(temp_dict)
-            return render_template("vitamin_info.html", vitamins = vitamins_list, vitamin=vitamin, top_10=data)
+            return render_template("vitamin_info.html", vitamins = vitamins_list, vitamin=vitamin, top_10=data, vitamin_text=vitamin_text)
 
-
-    
 
     if vitamin in vitamins_list:
         return render_template("vitamin_info.html", vitamins = vitamins_list, vitamin=vitamin)
