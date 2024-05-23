050ec53202107790ed66d25182463fd5c839bb69
Linda Fogli <foglilinda@gmail.com>
2024-05-23 13:41:30 +0200

    test_VitaminA

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,37 @@
 import pytest
 import requests
 from models import Fruit, db
+from app import app
 
 
 def test_route_home_local():
     response = requests.get('http://127.0.0.1:5000')
     assert response.status_code == 200
 
+def test_route_home_test_client():
+	client = app.test_client()
+	response = client.get('/')
+	assert response.status_code == 200
+
 def test_route_home_pythonanywhere():
     response = requests.get("https://ramia128.pythonanywhere.com/")
     assert response.status_code == 200
 
 def test_vitaminA():
+    with app.app_context():
+        data = Fruit.query.order_by((getattr(Fruit, "Vitamin_A_RE_per_mikrog")).desc()).limit(10).first() 
+
+    assert data.Namn == "Morot"
+
+def test_vitaminA_online():
     response = requests.get('http://127.0.0.1:5000/vitaminer/Vitamin A')
+    assert response.status_code == 200
 
-    # Test in some way that you get vitamin A stuff
+    with app.app_context():
+        data = Fruit.query.order_by((getattr(Fruit, "Vitamin_A_RE_per_mikrog")).desc()).limit(10).first() 
 
-    # Query database
-    data = Fruit.query.order_by((getattr(Fruit, "Vitamin_A_RE_per_mikrog")).desc()).limit(10).all() 
+    print(data.Namn, type(response.text))
 
+    assert "Morot" in response.text
 
-    print(response)
 
